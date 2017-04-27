272195d3985716c920912c2281600f1babea818c
Vladimir Zhdanov <vladimirzhdanov1992@gmail.com>
2017-04-27 08:23:12 +0600

    update requirements.txt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os.path import join, dirname
 
 setup(
-    name='kp_uglyparser',
+    name='kpuglyparser',
     version='1.0',
     packages=find_packages(),
     install_requires=[
